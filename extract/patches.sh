d669d78f717c884b45fa4eef9bb3fdb0ccc330e5
Aaron Kling <webgeek1234@gmail.com>
2021-11-25 15:45:16 -0600

    extract: Move tx2 xusb patch to t186-common [2/2]
    Change-Id: I501e6da6ea38f8bea9f25194f72df6239306fb7e

--- extract/patches.sh
+++ extract/patches.sh
@@ -1,18 +1,21 @@
-#
 # Copyright (C) 2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-FILELIST_PATHS+=("t186-common");
-PATCH_PATHS+=("t186-common");
+# Patch tx2 xusb firmware to fix issues with usb hubs. Sourced from:
+# https://forums.developer.nvidia.com/t/xbox-360-controller-through-usb-hub-broken-in-jetpack-4-x-works-in-jetpack-3-x-not-enough-host-c/110621/2
+function patch_tx2_xusb() {
+  git -C ${LINEAGE_ROOT}/${OUTDIR} apply ${LINEAGE_ROOT}/device/nvidia/t186-common/extract/tx2_xusb.patch
+}
+
+patch_tx2_xusb;
