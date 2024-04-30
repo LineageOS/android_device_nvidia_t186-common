f022130427d1c7102b5027aef82275c0be0d10d3
Aaron Kling <webgeek1234@gmail.com>
2024-04-30 01:32:00 -0500

    Copy xusb firmware to boot ramdisk
    With system as root removed, usb firmware should be available in ramdisk
    to allow boot modules to access it.
    
    Change-Id: I7c3e9391ccb8593ebfba59165f61763f8d0094bf

--- vendor/r32/t186-recovery.mk
+++ vendor/r32/t186-recovery.mk
@@ -17,8 +17,10 @@ T186_FIRMWARE_PATH := vendor/nvidia/t186/r32/firmware
 # Firmware
 ifeq ($(TARGET_TEGRA_KERNEL),4.9)
 PRODUCT_COPY_FILES += \
-    $(T186_FIRMWARE_PATH)/xusb/tegra18x_xusb_firmware:recovery/root/lib/firmware/tegra18x_xusb_firmware
+    $(T186_FIRMWARE_PATH)/xusb/tegra18x_xusb_firmware:recovery/root/lib/firmware/tegra18x_xusb_firmware \
+    $(T186_FIRMWARE_PATH)/xusb/tegra18x_xusb_firmware:$(TARGET_COPY_OUT_RAMDISK)/lib/firmware/tegra18x_xusb_firmware
 else
 PRODUCT_COPY_FILES += \
-    $(T186_FIRMWARE_PATH)/xusb/tegra18x_xusb_firmware:recovery/root/lib/firmware/nvidia/tegra186/xusb.bin
+    $(T186_FIRMWARE_PATH)/xusb/tegra18x_xusb_firmware:recovery/root/lib/firmware/nvidia/tegra186/xusb.bin \
+    $(T186_FIRMWARE_PATH)/xusb/tegra18x_xusb_firmware:$(TARGET_COPY_OUT_RAMDISK)/lib/firmware/nvidia/tegra186/xusb.bin
 endif
