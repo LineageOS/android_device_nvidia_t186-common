86c0df1d0a2de1eafa027966fa2a96ce66f99866
Aaron Kling <webgeek1234@gmail.com>
2024-12-16 23:05:31 -0600

    vendor: Use copy-files for firmware targets
    There's no real build time checks that the build system can do for these
    type of files. Plus duplicate copy-files entries are now a fatal error,
    so there's no benefit to keeping these as modules.
    
    Change-Id: Ib21e8f5b0ee03af6f83c62d84939aa5d2aeb6b6d

--- vendor/linux-firmware/t186.mk
+++ vendor/linux-firmware/t186.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2023-2024 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,34 +12,41 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := device/nvidia/t186-common/vendor/linux-firmware
+T186_FIRMWARE_PATH := kernel/nvidia/linux-firmware
 
-include $(LOCAL_PATH)/t186-recovery.mk
+DOWNSTREAM_BRANCH    := r32
+T186_DOWNSTREAM_PATH := vendor/nvidia/t186/$(DOWNSTREAM_BRANCH)/firmware
 
 # GPU firmware
-PRODUCT_PACKAGES += \
-    bl.bin \
-    ucode_load.bin \
-    fecs_bl.bin \
-    fecs_data.bin \
-    fecs_inst.bin \
-    fecs_sig.bin \
-    gpccs_bl.bin \
-    gpccs_data.bin \
-    gpccs_inst.bin \
-    gpccs_sig.bin \
-    sw_bundle_init.bin \
-    sw_method_init.bin \
-    sw_ctx.bin \
-    sw_nonctx.bin \
-    desc.bin \
-    image.bin \
-    sig.bin
+PRODUCT_COPY_FILES += \
+    $(T186_FIRMWARE_PATH)/nvidia/gp10b/acr/bl.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/acr/bl.bin \
+    $(T186_FIRMWARE_PATH)/nvidia/gp10b/acr/ucode_load.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/acr/ucode_load.bin \
+    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/fecs_bl.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/fecs_bl.bin \
+    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/fecs_data.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/fecs_data.bin \
+    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/fecs_inst.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/fecs_inst.bin \
+    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/fecs_sig.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/fecs_sig.bin \
+    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/fecs_sig.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/fecs_sig.bin \
+    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/gpccs_data.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/gpccs_data.bin \
+    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/gpccs_inst.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/gpccs_inst.bin \
+    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/gpccs_sig.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/gpccs_sig.bin \
+    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/sw_bundle_init.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/sw_bundle_init.bin \
+    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/sw_method_init.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/sw_method_init.bin \
+    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/sw_ctx.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/sw_ctx.bin \
+    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/sw_nonctx.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/sw_nonctx.bin \
+    $(T186_FIRMWARE_PATH)/nvidia/gp10b/pmu/desc.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/pmu/desc.bin \
+    $(T186_FIRMWARE_PATH)/nvidia/gp10b/pmu/image.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/pmu/image.bin \
+    $(T186_FIRMWARE_PATH)/nvidia/gp10b/pmu/sig.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/pmu/sig.bin
 
 # General firmware
-PRODUCT_PACKAGES += \
-    nvdec.bin \
-    nvenc.bin \
-    nvjpg.bin \
-    vic04_ucode.bin \
-    xusb.bin
+PRODUCT_COPY_FILES += \
+    $(T186_DOWNSTREAM_PATH)/tegra18x/nvhost_nvdec030_ns.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra186/nvdec.bin \
+    $(T186_DOWNSTREAM_PATH)/tegra18x/nvhost_nvenc061.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra186/nvenc.bin \
+    $(T186_DOWNSTREAM_PATH)/tegra18x/nvhost_nvjpg011.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra186/nvjpg.bin \
+    $(T186_FIRMWARE_PATH)/nvidia/tegra186/vic04_ucode.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra186/vic04_ucode.bin \
+    $(T186_FIRMWARE_PATH)/nvidia/tegra186/xusb.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra186/xusb.bin
+
+
+# Recovery / Boot Ramdisk
+PRODUCT_COPY_FILES += \
+    $(T186_FIRMWARE_PATH)/nvidia/tegra186/xusb.bin:recovery/root/lib/firmware/nvidia/tegra186/xusb.bin \
+    $(T186_FIRMWARE_PATH)/nvidia/tegra186/xusb.bin:$(TARGET_COPY_OUT_RAMDISK)/lib/firmware/nvidia/tegra186/xusb.bin
