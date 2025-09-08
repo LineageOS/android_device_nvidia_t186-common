fd4a2f466f8a0c294139795966fd155d18a252d6
Aaron Kling <webgeek1234@gmail.com>
2025-09-07 22:21:08 -0500

    linux-firmware: Use aosp style linux-firmware
    Change-Id: I3b9b76189cf866ad6ef1983164a6cc4de3d5ce73

--- vendor/linux-firmware/t186.mk
+++ vendor/linux-firmware/t186.mk
@@ -12,39 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-T186_FIRMWARE_PATH := kernel/nvidia/linux-firmware
+T186_FIRMWARE_PATH := external/linux-firmware-mainline/firmware
 
 DOWNSTREAM_BRANCH    := r32
 T186_DOWNSTREAM_PATH := vendor/nvidia/t186/$(DOWNSTREAM_BRANCH)/firmware
 
-# GPU firmware
-PRODUCT_COPY_FILES += \
-    $(T186_FIRMWARE_PATH)/nvidia/gp10b/acr/bl.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/acr/bl.bin \
-    $(T186_FIRMWARE_PATH)/nvidia/gp10b/acr/ucode_load.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/acr/ucode_load.bin \
-    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/fecs_bl.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/fecs_bl.bin \
-    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/fecs_data.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/fecs_data.bin \
-    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/fecs_inst.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/fecs_inst.bin \
-    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/fecs_sig.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/fecs_sig.bin \
-    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/fecs_sig.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/fecs_sig.bin \
-    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/gpccs_bl.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/gpccs_bl.bin \
-    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/gpccs_data.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/gpccs_data.bin \
-    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/gpccs_inst.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/gpccs_inst.bin \
-    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/gpccs_sig.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/gpccs_sig.bin \
-    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/sw_bundle_init.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/sw_bundle_init.bin \
-    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/sw_method_init.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/sw_method_init.bin \
-    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/sw_ctx.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/sw_ctx.bin \
-    $(T186_FIRMWARE_PATH)/nvidia/gp10b/gr/sw_nonctx.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/gr/sw_nonctx.bin \
-    $(T186_FIRMWARE_PATH)/nvidia/gp10b/pmu/desc.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/pmu/desc.bin \
-    $(T186_FIRMWARE_PATH)/nvidia/gp10b/pmu/image.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/pmu/image.bin \
-    $(T186_FIRMWARE_PATH)/nvidia/gp10b/pmu/sig.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gp10b/pmu/sig.bin
+# Upstream firmware
+PRODUCT_PACKAGES += \
+    linux_firmware_nvidia-tegra186
 
 # General firmware
 PRODUCT_COPY_FILES += \
     $(T186_DOWNSTREAM_PATH)/tegra18x/nvhost_nvdec030_ns.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra186/nvdec.bin \
     $(T186_DOWNSTREAM_PATH)/tegra18x/nvhost_nvenc061.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra186/nvenc.bin \
-    $(T186_DOWNSTREAM_PATH)/tegra18x/nvhost_nvjpg011.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra186/nvjpg.bin \
-    $(T186_FIRMWARE_PATH)/nvidia/tegra186/vic04_ucode.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra186/vic04_ucode.bin \
-    $(T186_FIRMWARE_PATH)/nvidia/tegra186/xusb.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra186/xusb.bin
+    $(T186_DOWNSTREAM_PATH)/tegra18x/nvhost_nvjpg011.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra186/nvjpg.bin
 
 
 # Recovery / Boot Ramdisk
